fed23410b2c8b3a37bae3ee2bf5caa4b9f05970e
stms <2863345871@qq.com>
2020-03-09 22:58:49 +0800

    初次提交

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Jaihk662;
+using Google.Protobuf;
+using System.IO;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestForProto testForProto = new TestForProto();
+        testForProto.Name = "stms";
+        Debug.Log(testForProto.ToByteArray());
+        Debug.Log(Application.dataPath);
+        FileStream fileStream = new FileStream(Application.dataPath + "/local/file.txt", FileMode.OpenOrCreate, FileAccess.Write);
+        StreamWriter streamWriter = new StreamWriter(fileStream);
+        //fileStream.
+        //streamWriter.Write("ddddddddddddddddd");
+        fileStream.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
